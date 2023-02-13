7e114fff33d4d72b6f3e99973c4930e247e5e863
yulescrob <122058516+yulescrob@users.noreply.github.com>
2023-02-12 20:33:47 -0500

    finalized

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        double itemPrice;
+        double shippingCost;
+        double totalPrice;
 
+        //simulate input choice
+        itemPrice = 33;
 
-
-
-
-
-
-
+        if (itemPrice >= 100) {
+            System.out.println("You quality for free shipping. Your total price is " + itemPrice);
+        }
+        else {
+            //calculations
+            shippingCost = itemPrice * 0.02;
+            totalPrice = itemPrice + shippingCost;
+            System.out.println("Your shipping cost is " + shippingCost + " and your total price is " + totalPrice);
+        }
 
     }
 }
